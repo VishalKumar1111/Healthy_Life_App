b89fa2ff97278bf27ca2067c12fb196b99e249c3
VISHAL <singh01vishal01@gmail.com>
2022-05-12 16:50:06 +0530

    New Add

---
+++ app/src/main/java/com/example/Doctor/Next/QuestionActivity/No.kt
@@ -0,0 +1,12 @@
+package com.example.Doctor.Next.QuestionActivity
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.Doctor.R
+
+class No:Fragment(R.layout.fragment_rule) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/Doctor/Next/QuestionActivity/Question.kt
+++ app/src/main/java/com/example/Doctor/Next/QuestionActivity/Question.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import com.example.Doctor.R
 
 
@@ -14,6 +15,7 @@ class Question : Fragment(R.layout.fragment_question2) {
 
     private var count=0
     var score=0
+    var low=0
     private var next=0
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -25,59 +27,59 @@ class Question : Fragment(R.layout.fragment_question2) {
 
         button.setOnClickListener {
             count++
-            var score=0
+            score++
             if(count==1)
             {
                 text.text="Is you ARe Felling Depressed"
-                score+=1
+
             }
             if(count==2)
             {
                 text.text="Are you Felling cold"
-                score+=1
+
             }
             if(count==3)
             {
                 text.text="Disappointment at home, work, or school (in teens, this may be breaking up with a boyfriend or girlfriend, failing a class or parents divorcing)"
-                score+=1
+
 
             }
             if (count==4)
             {
                 text.text="Do you often oversleep? Do you think you get too little sleep (and may have insomnia)?"
-                score+=1
+
             }
             if (count==5){
                 text.text="Do you have aches and pains?"
-                score+=1
+
             }
-            /*    if (count>3)
+                if (score>3)
                {
-                    findNavController().navigate(R.id.action_fourth_to_yes)
+                    findNavController().navigate(R.id.action_question_to_yes)
                 }
-        */
+
         }
         button2.setOnClickListener {
             next++
+            low++
             if (next==1){
                 text.text="Have parents, other relatives, or maybe even friends accused you of being “irritated,” “nasty,” or “always in a bad mood?"
-                score-+1
             }
             if (next==2){
                 text.text="Does life seem pointless?"
-                score-=1
             }
             if(next==3){
                 text.text="Does it seem like it’s impossible to concentrate?"
-                score-=1
             }
             if (next==4){
                 text.text="Have you withdrawn from your friends and family?"
-                score-=1
             }
             if (next==5){
                 text.text="Have you noticed a sudden change in your weight?"
-                score-=1
+            }
+            if (low>3){
+                findNavController().navigate(R.id.action_question_to_no)
+
             }
         }
 
